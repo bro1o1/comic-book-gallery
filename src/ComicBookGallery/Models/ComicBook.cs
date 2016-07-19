79fcb5668c86425ba2a2a1461528ed4365812bc4
BG <BG@BG>
2016-07-19 16:28:52 +0300

    Updated the Comic Book Detail view layout

--- src/ComicBookGallery/Models/ComicBook.cs
+++ src/ComicBookGallery/Models/ComicBook.cs
@@ -7,28 +7,27 @@ namespace ComicBookGallery.Models
 {
     public class ComicBook
     {
-        public int ID { get; set; }
+        public int Id { get; set; }
         public string SeriesTitle { get; set; }
         public int IssueNumber { get; set; }
-        public String DescriptionHtml { get; set; }
+        public string DescriptionHtml { get; set; }
         public Artist[] Artists { get; set; }
-        public bool Favoruite { get; set; }
+        public bool Favorite { get; set; }
 
         public string DisplayText
         {
             get
             {
-                return '"' + SeriesTitle + " #" + IssueNumber; 
+                return SeriesTitle + " #" + IssueNumber;
             }
         }
 
-        //series-title-issuenumber.jpg
+        // series-title-issuenumber.jpg
         public string CoverImageFileName
         {
             get
             {
-                return SeriesTitle.Replace(" ", "-")
-                    .ToLower() + "-" + IssueNumber + ".jpg";
+                return SeriesTitle.Replace(" ", "-").ToLower() + "-" + IssueNumber + ".jpg";
             }
         }
     }
